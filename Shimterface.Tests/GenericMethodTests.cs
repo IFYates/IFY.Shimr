ac61468d1d19cc129f64725804bf972da21e699e
Ian Yates <ian.yates@neofintechlab.com>
2021-03-22 09:55:35 +0000

    Proof of issue #5

---
+++ Shimterface.Tests/GenericMethodTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shimterface.Tests
+{
+    // https://github.com/IanYates83/Shimterface/issues/5
+    [TestClass]
+    public class GenericMethodTests
+    {
+        public class TestClass
+        {
+            public void Test<T>()
+            {
+            }
+        }
+
+        public interface ITestShim
+        {
+            void Test<T>();
+        }
+
+        [TestMethod]
+        public void Facade_can_include_generic_methods()
+        {
+            var shim = ShimBuilder.Shim<ITestShim>(new TestClass());
+            shim.Test<string>();
+        }
+    }
+}
