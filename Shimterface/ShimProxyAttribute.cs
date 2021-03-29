046f54b1f82cb73745cfbe78ea48474203c1640c
Ian Yates <ian.yates@neofintechlab.com>
2021-03-29 19:30:56 +0100

    Tests for #3

---
+++ Shimterface/ShimProxyAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Shimterface
+{
+	/// <summary>
+	/// Mark a shim member as being a proxy to an implementation elsewhere.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = false)]
+	public class ShimProxyAttribute : Attribute
+	{
+		/// <summary>
+		/// The type that implements this member.
+		/// </summary>
+        public Type ImplementationType { get; }
+		/// <summary>
+		/// The behaviour of this proxy member.
+		/// </summary>
+        public ProxyBehaviour Behaviour { get; }
+
+        public ShimProxyAttribute(Type implType)
+			: this (implType, ProxyBehaviour.Default)
+		{
+        }
+        public ShimProxyAttribute(Type implType, ProxyBehaviour behaviour)
+		{
+            ImplementationType = implType;
+            Behaviour = behaviour;
+        }
+	}
+}
