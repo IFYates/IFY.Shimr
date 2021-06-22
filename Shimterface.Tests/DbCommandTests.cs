54c256d83fb5e9b1155864d05bd679e646011514
Ian Yates <ian.yates@neofintechlab.com>
2021-06-22 08:30:54 +0100

    Tests for hidden member issue

---
+++ Shimterface.Tests/DbCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Shimterface.Tests
+{
+    // Specific issues around these structures
+    [TestClass]
+	public class DbCommandTests
+	{
+		public interface ITestDbCommand : IDbCommand
+		{
+            DbTransaction DbTransaction { get; set; } // From DbCommand
+        }
+
+        [TestMethod]
+        public void Can_shim_DbCommand()
+        {
+            var obj = new SqlCommand();
+
+            var shim = obj.Shim<ITestDbCommand>();
+        }
+	}
+}
