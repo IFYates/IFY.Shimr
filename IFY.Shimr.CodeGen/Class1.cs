043e2d379212b9952025f254422281066cdc528f
IFYates <ian@iyates.co.uk>
2024-07-08 15:25:57 +0100

    Updated all libraries

--- IFY.Shimr.CodeGen/Class1.cs
+++ IFY.Shimr.CodeGen/Class1.cs
@@ -5,83 +5,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace IFY.Shimr.CodeGen
+namespace IFY.Shimr.CodeGen;
+
+public static class TypeDeclarationSyntaxExtensions
 {
-    public static class TypeDeclarationSyntaxExtensions
+    const char NESTED_CLASS_DELIMITER = '+';
+    const char NAMESPACE_CLASS_DELIMITER = '.';
+    const char TYPEPARAMETER_CLASS_DELIMITER = '`';
+
+    public static string GetFullName(this TypeDeclarationSyntax source, bool asParent = false)
     {
-        const char NESTED_CLASS_DELIMITER = '+';
-        const char NAMESPACE_CLASS_DELIMITER = '.';
-        const char TYPEPARAMETER_CLASS_DELIMITER = '`';
-
-        public static string GetFullName(this TypeDeclarationSyntax source, bool asParent = false)
+        if (source is null)
         {
-            if (source is null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
+            throw new ArgumentNullException(nameof(source));
+        }
 
-            var namespaces = new LinkedList<NamespaceDeclarationSyntax>();
-            var types = new LinkedList<TypeDeclarationSyntax>();
-            for (var parent = source.Parent; parent is object; parent = parent.Parent)
+        var namespaces = new LinkedList<NamespaceDeclarationSyntax>();
+        var types = new LinkedList<TypeDeclarationSyntax>();
+        for (var parent = source.Parent; parent is not null; parent = parent.Parent)
+        {
+            if (parent is NamespaceDeclarationSyntax @namespace)
             {
-                if (parent is NamespaceDeclarationSyntax @namespace)
-                {
-                    namespaces.AddFirst(@namespace);
-                }
-                else if (parent is TypeDeclarationSyntax type)
-                {
-                    types.AddFirst(type);
-                }
+                namespaces.AddFirst(@namespace);
             }
-
-            var result = new StringBuilder();
-            for (var item = namespaces.First; item is object; item = item.Next)
+            else if (parent is TypeDeclarationSyntax type)
             {
-                result.Append(item.Value.Name).Append(NAMESPACE_CLASS_DELIMITER);
+                types.AddFirst(type);
             }
-            for (var item = types.First; item is object; item = item.Next)
-            {
-                appendName(item.Value);
-                result.Append(asParent ? NAMESPACE_CLASS_DELIMITER : NESTED_CLASS_DELIMITER);
-            }
-            appendName(source);
+        }
 
-            return result.ToString();
+        var result = new StringBuilder();
+        for (var item = namespaces.First; item is not null; item = item.Next)
+        {
+            result.Append(item.Value.Name).Append(NAMESPACE_CLASS_DELIMITER);
+        }
+        for (var item = types.First; item is not null; item = item.Next)
+        {
+            appendName(item.Value);
+            result.Append(asParent ? NAMESPACE_CLASS_DELIMITER : NESTED_CLASS_DELIMITER);
+        }
+        appendName(source);
 
-            void appendName(TypeDeclarationSyntax type)
+        return result.ToString();
+
+        void appendName(TypeDeclarationSyntax type)
+        {
+            result.Append(type.Identifier.Text);
+            var typeArguments = type.TypeParameterList?.ChildNodes()
+                .Count(node => node is TypeParameterSyntax) ?? 0;
+            if (typeArguments != 0)
             {
-                result.Append(type.Identifier.Text);
-                var typeArguments = type.TypeParameterList?.ChildNodes()
-                    .Count(node => node is TypeParameterSyntax) ?? 0;
-                if (typeArguments != 0)
-                {
-                    result.Append(TYPEPARAMETER_CLASS_DELIMITER).Append(typeArguments);
-                }
+                result.Append(TYPEPARAMETER_CLASS_DELIMITER).Append(typeArguments);
             }
         }
     }
+}
 
-    [Generator]
-    public class Class1 : ISourceGenerator
+[Generator]
+public class Class1 : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
     {
-        public void Execute(GeneratorExecutionContext context)
-        {
-            var attrType = Type.GetType("IFY.Shimr.ShimrAttribute");
+        var attrType = Type.GetType("IFY.Shimr.ShimrAttribute");
 
-            var attributeSymbol = context.Compilation.GetTypeByMetadataName("IFY.Shimr.ShimrAttribute");
-            var classesWithAttribute = context.Compilation.SyntaxTrees
-                .SelectMany(st => st.GetRoot()
-                        .DescendantNodes()
-                        .Where(n => n is InterfaceDeclarationSyntax)
-                        .Select(n => n as InterfaceDeclarationSyntax)
-                        .Where(r => r.AttributeLists
-                            .SelectMany(al => al.Attributes)
-                            .Any(a => a.Name.GetText().ToString() == "Shimr")));
+        var attributeSymbol = context.Compilation.GetTypeByMetadataName("IFY.Shimr.ShimrAttribute");
+        var classesWithAttribute = context.Compilation.SyntaxTrees
+            .SelectMany(st => st.GetRoot()
+                    .DescendantNodes()
+                    .Where(n => n is InterfaceDeclarationSyntax)
+                    .Select(n => n as InterfaceDeclarationSyntax)
+                    .Where(r => r.AttributeLists
+                        .SelectMany(al => al.Attributes)
+                        .Any(a => a.Name.GetText().ToString() == "Shimr")));
 
-            var cast = classesWithAttribute.Single().AttributeLists.Single().SyntaxTree.GetRoot().DescendantNodes()
+        var cast = classesWithAttribute.Single().AttributeLists.Single().SyntaxTree.GetRoot().DescendantNodes()
 .Where(n => n is TypeOfExpressionSyntax).Select(n => n as TypeOfExpressionSyntax).First();
 
-            string source = $@"// Auto-generated code
+        string source = $@"// Auto-generated code
 using System;
 
 namespace IFY.Shimr
@@ -124,23 +124,22 @@ return ""> {classesWithAttribute.Single().GetFullName()}"";
     }}
 }}
 ";
-            context.AddSource($"Shimr.g.cs", source);
-        }
-
-        public void Initialize(GeneratorInitializationContext context)
-        {
-            //context.RegisterForSyntaxNotifications(() => new ShimrReceiver());
-        }
-
-        //class ShimrReceiver : ISyntaxReceiver
-        //{
-        //    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-        //    {
-        //        if (syntaxNode is ClassDeclarationSyntax cds
-        //            && cds.HaveAttribute(ShimrAttribute))
-        //        {
-        //        }
-        //    }
-        //}
+        context.AddSource($"Shimr.g.cs", source);
     }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        //context.RegisterForSyntaxNotifications(() => new ShimrReceiver());
+    }
+
+    //class ShimrReceiver : ISyntaxReceiver
+    //{
+    //    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    //    {
+    //        if (syntaxNode is ClassDeclarationSyntax cds
+    //            && cds.HaveAttribute(ShimrAttribute))
+    //        {
+    //        }
+    //    }
+    //}
 }
